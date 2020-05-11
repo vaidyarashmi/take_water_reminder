e993e4e938de37d294e39c7009aebbcc9117deb2
Rashmi Vaidya <rashmianandvaidya@gmail.com>
2020-05-11 22:14:19 +0530

    Add files via upload

---
+++ Take_water_reminder.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__=="__main__":
+	notification.notify(
+		title="please drink water",
+		message="Plaese drink water is necessary for us",
+		timeout=100
+	
+	)
+	
